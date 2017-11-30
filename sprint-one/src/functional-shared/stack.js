c5d69d4ed36552cbaae014a04cbff5ab860a2a94
francocatania <catania.fco@gmail.com>
2017-11-29 20:48:13 -0800

    working on second Stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,18 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  _.extend(Stack, stackMethods);
+  // _.extend(this, stackMethods);
+  var instance = Object.create(stackMethods);
+  return instance;
 };
 
 var stackMethods = {
-  someInstance : {},
-  storage : {
-    
+  someInstance : {
   },
-  size : function(){
-    
+  storage : {
+  },
+  size : function() {
+    var someInstance = this.someInstance; 
+    return Object.keys(someInstance).length;
+  },
+  push : function(value){
+    var someInstance = this.someInstance;
+    someInstance[Object.keys(someInstance).length] = value;
   }
-
+  
 };
 
 
