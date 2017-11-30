52b0ee9b6b8ec3359da051cf1c4c7fc5110fb5bb
francocatania <catania.fco@gmail.com>
2017-11-29 16:32:29 -0800

    Functional - Complete

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,17 +2,42 @@ var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
-
+  var storage = {
+    index : -1,
+    queueIndex : 0
+  };
+  
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var index = storage.index + 1;
+    someInstance[index] = value; 
+    storage.index += 1;
   };
 
   someInstance.dequeue = function() {
+    // if (Object.keys(someInstance).length > 0){
+    //   var index = storage.queueIndex; 
+    //   var removedValue = someInstance[index];
+    //   delete someInstance[index];
+    //   storage.index --;
+    //   storage.queueIndex ++;
+    //   return removedValue;
+    // }
+    var index = storage.index;
+    var removedValue = undefined;
+    if (index >= 0) {
+      removedValue = someInstance['0'];
+      for (var i = 0; i <= index; i++) {
+         someInstance[i] = someInstance[i + 1];
+      }
+      storage.index --;
+    }
+    return removedValue;
   };
 
   someInstance.size = function() {
+    return storage.index + 1;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,17 +2,32 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {
+    index : -1
+  };
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var index = storage.index+1;
+    someInstance[index] = value;
+    storage.index += 1;
   };
 
   someInstance.pop = function() {
+    var index = storage.index;
+    var poppedValue = undefined;
+    if (index >= 0) {
+      poppedValue = someInstance[index];
+      delete someInstance[index];
+      storage.index -= 1;
+    }
+    return poppedValue; 
   };
 
   someInstance.size = function() {
+    return storage.index+1;
   };
 
   return someInstance;
 };
+
