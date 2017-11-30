7632204ca86ca86fe7aaf12f4ed6b00399b37c24
francocatania <catania.fco@gmail.com>
2017-11-30 11:42:20 -0800

    Finished both functional sharing

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,35 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {};
+  instance.storage = { index : -1};
+  instance.someInstance = {};
+  _.extend(instance, queueMethods);
+  return instance;
 };
 
 var queueMethods = {};
+
+queueMethods.size = function () {
+  return this.storage.index + 1;
+};
+
+queueMethods.enqueue = function(value) {
+  this.someInstance[this.storage.index + 1] = value;
+  this.storage.index ++;
+};
+
+queueMethods.dequeue = function() {
+  if (this.storage.index > -1) {
+    var removedValue = this.someInstance['0'];
+    for (var i = 0; i < this.storage.index; i++) {
+      this.someInstance[i] = this.someInstance[i + 1];
+    }
+    this.storage.index --;
+    return removedValue;
+  } else {
+    return 0;
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -2,24 +2,38 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   // _.extend(this, stackMethods);
-  var instance = Object.create(stackMethods);
-  return instance;
+  var stackInstance = {};  
+  stackInstance.storage = {};
+  stackInstance.someInstance = {};
+  _.extend(stackInstance, stackMethods);
+  return stackInstance;
 };
 
-var stackMethods = {
-  someInstance : {
-  },
-  storage : {
-  },
-  size : function() {
-    var someInstance = this.someInstance; 
-    return Object.keys(someInstance).length;
-  },
-  push : function(value){
-    var someInstance = this.someInstance;
-    someInstance[Object.keys(someInstance).length] = value;
-  }
-  
+var stackMethods = {}
+
+stackMethods.size = function() {
+  // var someInstance = this.someInstance; 
+  return Object.keys(this.someInstance).length;
 };
+
+stackMethods.push = function(value){
+  // var someInstance = this.someInstance;
+  this.someInstance[Object.keys(this.someInstance).length] = value;
+};
+
+stackMethods.pop = function(){
+  // var someInstance = this.someInstance;
+  if (Object.keys(this.someInstance).length > 0){
+    var removedValue = this.someInstance[Object.keys(this.someInstance).length - 1];
+    delete this.someInstance[Object.keys(this.someInstance).length - 1];
+    return removedValue;
+  } else {
+    return undefined
+  }
+};
+
+
+console.log(Stack);
+
 
 
