2ba6f70b7092b6bc0100083ff04df881d75a37fe
francocatania <catania.fco@gmail.com>
2017-11-30 18:26:54 -0800

    Finished Sprint One

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = { index : -1};
+  this.someInstance = {};
+};
+
+Queue.prototype.size = function() {
+  return this.storage.index + 1;
+};
+
+Queue.prototype.enqueue = function (value) {
+  this.someInstance[this.storage.index + 1] = value;
+  this.storage.index ++;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.storage.index > -1) {
+    var removedVal = this.someInstance['0'];
+    for (var i = 0; i < this.storage.index; i++ ) {
+      this.someInstance[i] = this.someInstance[i + 1];
+    }
+    delete this.someInstance[this.storage.index]
+    this.storage.index--;
+    return removedVal;
+  } else {
+    return 0;
+  }
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.storage = {};
+  this.someInstance = {};
 };
 
+Stack.prototype.size = function() {
+  return Object.keys(this.someInstance).length;
+};
 
+Stack.prototype.push = function(value) {
+  this.someInstance[Object.keys(this.someInstance).length] = value;
+};
+
+Stack.prototype.pop = function() {
+  if (this.size() > 0) {
+    var removed = this.someInstance[Object.keys(this.someInstance).length - 1];
+    delete this.someInstance[Object.keys(this.someInstance).length - 1];
+    return removed;
+  } else {
+    return 0;
+  }
+};
